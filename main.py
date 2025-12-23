92d16cc4b53a5fe81d7141c8ff5b8d3c3bc99da5
revo <revo@gmail.com>
2025-12-23 21:54:28 +0530

    floor with debug text box added for live numbers

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ width, height = 800, 600
 fps = 60
 bgcolor = (25, 25, 25)
 circle_color = (200, 255, 255)
+floor_color = (200, 200, 200)
 
-GRAVITY = 1000 #pixels per second squared
+gravity = 1000 #pixels per second squared
+restitution = 0.4
 
 # -----------------------------
 # Initialization
@@ -19,6 +21,8 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("2D Physics Engine")
 clock = pygame.time.Clock()
 
+font = pygame.font.SysFont(None, 24)
+
 # -----------------------------
 # Physics state
 # -----------------------------
@@ -26,6 +30,8 @@ x = width // 2
 y = 100          # start near the top
 vy = 0           # vertical velocity (px/s)
 radius = 30
+
+floor_y = height - 50
 
 # -----------------------------
 # Main Loop
@@ -41,11 +47,28 @@ while running:
             running = False
 
     # --- Physics update ---
-    vy += GRAVITY * dt      # v = v + a·dt
+    vy += gravity * dt      # v = v + a·dt
     y += vy * dt            # y = y + v·dt
+     # Floor collision
+    if y + radius >= floor_y:
+        y = floor_y - radius
+        vy = -vy * restitution
+
+        # stop tiny jitter
+        if abs(vy) < 5:
+            vy = 0
 
     # --- Render ---
     screen.fill(bgcolor)
+
+     # Draw floor
+    pygame.draw.line(
+        screen,
+        floor_color,
+        (0, floor_y),
+        (width, floor_y),
+        2
+    )
 
     pygame.draw.circle(
         screen,
@@ -54,10 +77,23 @@ while running:
         radius
     )
 
-    if y - radius > height:
+    if vy == 0:
         y = 100
-        vy = 0
 
+    # --- Debug text ---
+    debug_lines = [
+    f"vx: 0",
+    f"vy: {vy:.1f}",
+    f"g: {gravity}",
+    f"restitution: {restitution}"
+]
+
+    y_offset = 10
+    for line in debug_lines:
+        text_surface = font.render(line, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(topright=(width - 10, y_offset))
+        screen.blit(text_surface, text_rect)
+        y_offset += text_surface.get_height() + 2
 
     pygame.display.flip()
 
